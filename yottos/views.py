e8bd80865e505f777f83f661e871f59042a36d82
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2020-02-03 10:32:10 +0200

    init

--- yottos/views.py
+++ yottos/views.py
@@ -1,11 +1,8 @@
 # coding=utf-8
 import json
-import urllib
-import urllib2
 
 from pyramid.httpexceptions import HTTPMovedPermanently
 from pyramid.view import notfound_view_config, view_config
-
 
 
 @view_config(route_name='index', renderer='templates/index.pt')
